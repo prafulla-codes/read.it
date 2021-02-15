57ef20188084e3177b0ff3c08018e8c9c23eed16
Prafulla R <prafzzraichurkar1@gmail.com>
2021-02-15 22:34:14 +0530

    Added Styled Components

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from '../../Components/Navbar';
 import TotalBooksRead from '../../Components/TotalBooksRead';
 const Dashboard = () => {
 
     return (
         <div>
+            <Navbar/>
             Dashboard : <TotalBooksRead/>
 
         </div>
