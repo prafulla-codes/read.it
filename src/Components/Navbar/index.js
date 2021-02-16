81bd273f58e091cbf67a041e12923184bd1e8c6c
Prafulla R <prafzzraichurkar1@gmail.com>
2021-02-16 22:57:59 +0530

    Worked on navbar, added fonts and sizes

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 import COLORS from '../../Constants/Colors';
+import SIZES from '../../Constants/Sizes';
+import FONTS from '../../Constants/Fonts';
 const NavbarStyle = styled.div`
 width:100%;
+font-family:${FONTS.oregano};
+height:${SIZES.navbar.height}rem;
+color:${COLORS.primaryText};
+padding:${SIZES.navbar.padding}rem;
 background:${COLORS.primary}
 `
 
--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar';
 import TotalBooksRead from '../../Components/TotalBooksRead';
+
 const Dashboard = () => {
 
     return (
-        <div>
+        <div id="dashboard">
             <Navbar/>
             Dashboard : <TotalBooksRead/>
 
